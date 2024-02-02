735e1537aedd6acee610eb78ec7d32951f9f8712
Hackett <2579585529@qq.com>
2024-02-02 10:43:35 +0800

    upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const Applications());
-}
+void main() => runApp(const Application());
 
-class Applications extends StatelessWidget {
-  const Applications({super.key});
+class Application extends StatelessWidget {
+  const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.blueGrey),
+        scaffoldBackgroundColor: Colors.blueGrey.shade100,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+          brightness: Brightness.light,
+        ),
       ),
-      home: const Center(
-        child: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          decoration: BoxDecoration(color: Colors.blueGrey.shade200),
+          child: FittedBox(
+            child: Text(
+              'Hello world!'.toUpperCase(),
+              style: GoogleFonts.rubikBurned(fontSize: 120.0, color: Colors.blueGrey),
+            ),
+          ),
+        ),
       ),
     );
   }
